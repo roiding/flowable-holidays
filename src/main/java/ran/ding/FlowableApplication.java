7c75a165e0bff594149686a9cfa95223314d8b4e
roiding <maodoulove19950815@gmail.com>
2021-04-30 15:06:11 +0800

    初始提交

---
+++ src/main/java/ran/ding/FlowableApplication.java
@@ -0,0 +1,13 @@
+package ran.ding;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author roiding
+ */
+@SpringBootApplication
+public class FlowableApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableApplication.class,args);
+    }
+}
